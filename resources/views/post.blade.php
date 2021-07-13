ba5f9a9e3203dd6c7f0cec59f0117d433c792fe9
Евгений Панкрашкин <69710763+Noxeeql@users.noreply.github.com>
2021-07-13 17:41:10 +0500

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -12,43 +12,24 @@
             <p class="text-justify">{{ $data->text }}</p>
         </div>
 
-        @foreach($data->comments as $comment)
-            <div class="alert alert-info position-static col-lg-8 mx-auto" align="center">
-
-                {{-- <p>{{ $comment->user_id }}</p> --}}
-                <p>{{ $user->name }}</p>
-                <p>{{ $comment->text }}</p>
-            </div>
-        @endforeach
-        <h4 class="text-center">Add comment</h4>
-
-            {{-- <form>
-             <div class="form-group row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-                <div class="col-sm-10">
-                    <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{ $data->id}}">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                    <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-                    </div>
-                </div>
-                </form> --}}
-
-
-                    <form method="post" action="{{ route('comment.add') }}">
-                        @csrf
-                        <div class="form-group col-lg-8 mx-auto" >
-                            <input type="text" name="text" class="form-control" />
-                            <input type="hidden" name="news_id" value="{{ $data->id }}" />
-                        </div>
-                        <div class="form-group row col-lg-1 mx-auto">
-                            <input type="submit" class="btn btn-success" value="Add Comment" align="enter" style="margin-top:10px;" />
-                        </div>
-
-@include('layouts.footer')
+    @foreach($data->comments as $comment)
+        <div class="alert alert-info position-static col-lg-8 mx-auto" align="center">
+             {{-- <p>{{ $comment->user_id }}</p> --}}
+             <p>{{ $user->name }}</p>
+             <p>{{ $comment->text }}</p>
+        </div>
+    @endforeach
+    <h4 class="text-center">Add comment</h4>
+        <form method="post" action="{{ route('comment.add') }}">
+              @csrf
+              <div class="form-group col-lg-8 mx-auto" >
+                    <input type="text" name="text" class="form-control" />
+                    <input type="hidden" name="news_id" value="{{ $data->id }}" />
+              </div>
+              <div class="form-group row col-lg-1 mx-auto">
+                    <input type="submit" class="btn btn-success" value="Add Comment" align="enter" style="margin-top:10px;" />
+              </div>
+     @include('layouts.footer')
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
